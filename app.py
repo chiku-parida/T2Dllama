7448054f46cadde8d061de26e0680f37ac5c4b44
CHIKU PARIDA <126141216+chiku-parida@users.noreply.github.com>
2024-05-09 16:29:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,16 +12,12 @@ from llama_index.llms.llama_cpp.llama_utils import (
 )
 llm = LlamaCPP(
     #model_url='https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q4_K_M.gguf',
-    model_path='/home/charles/phd_project/LLM_H/mistral-7b-instruct-v0.1.Q4_K_M.gguf',
+    model_path='/.../mistral-7b-instruct-v0.1.Q4_K_M.gguf', #model path if you downloaded the model from above link
     temperature=0.1,
     max_new_tokens=256,
     context_window=3900,
-    # kwargs to pass to __call__()
     generate_kwargs={},
-    # kwargs to pass to __init__()
-    # set to at least 1 to use GPU
     model_kwargs={"n_gpu_layers": -1},
-    # transform inputs into Llama2 format
     messages_to_prompt=messages_to_prompt,
     completion_to_prompt=completion_to_prompt,
     verbose=False,
